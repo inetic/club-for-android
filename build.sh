e9294e6f09eef7df214482d4d9f5b810790a4633
Peter Jankuliak <p.jankuliak@gmail.com>
2016-06-27 16:01:51 +0200

    Do git pull if club already cloned

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+set -e
+
 if [ "$1" = "clean" ]; then
   rm -rf libs obj club 2>/dev/null
   rm -rf jni/club 2>/dev/null
   exit
 fi
 
-git clone git://github.com/inetic/club jni/club
+if [ -d jni/club ]; then
+  (cd jni/club; git pull)
+else
+  git clone git://github.com/inetic/club jni/club
+fi
+
 ndk-build
 
